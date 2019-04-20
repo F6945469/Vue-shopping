ad032df333eb961e6bb2aea6aa8f5ad78323fd59
yzbgyq <710851698@qq.com>
2019-04-20 22:41:12 +0800

    修复滑动切换城市的bug

--- vue/src/components/city/CityRight.vue
+++ vue/src/components/city/CityRight.vue
@@ -1,55 +1,53 @@
 <template>
-    <ul>
-        <li v-for="val in citiesList" 
-            :key="val" @click="city" 
-            @touchstart.prevent="hiddleTouchstat" 
-            @touchmove="hiddleTouchmove"
-            @touchend="hiddleTouchend"
-            :ref="val"
-         >{{val}}</li>
+    <ul ref="ul">
+        <li v-for="val in citiesList" :key="val" @click="city" @touchstart.prevent="hiddleTouchstat" @touchmove="hiddleTouchmove" @touchend="hiddleTouchend" :ref="val">{{val}}</li>
     </ul>
 </template>
 
 <script>
 export default {
-  props: ["cities"],
-  data() {
-    return {
-      touchStatus: false //标识符
-    };
-  },
-  computed: {
-    citiesList() {
-      let arr = []
-      for (const k of Object.keys(this.cities)) {
-          arr.push(k)
-      }
-      return arr
-      
-    }
-  },
-  methods: {
-    city(e) {
-      // 向外触发事件
-      this.$emit("change", e.target.innerHTML);
+    props: ["cities"],
+    data() {
+        return {
+            touchStatus: false //标识符
+        };
     },
-    hiddleTouchstat() {
-      this.touchStatus = true;
+    computed: {
+        citiesList() {
+            let arr = []
+            for (const k of Object.keys(this.cities)) {
+                arr.push(k)
+            }
+            return arr
+        },
+
+
     },
-    hiddleTouchmove(e) {
-      if (this.touchStatus) {
-        const startY = this.$refs["A"][0].offsetTop; //A 距离顶部的高度
-        const touchY = e.touches[0].clientY - 83;
-        const index = Math.floor((touchY - startY) / 25); //当前手指滑动的位置
-        if (index >= 0 && this.citiesList.length) {
-          this.$emit("change", this.citiesList[index]);
+    methods: {
+        city(e) {
+            // 向外触发事件
+            this.$emit("change", e.target.innerHTML);
+        },
+        hiddleTouchstat() {
+            this.touchStatus = true;
+        },
+        hiddleTouchmove(e) {
+            if (this.touchStatus) {
+                const startY = this.$refs["A"][0].offsetTop; //A 距离顶部的高度
+                const eleOffsetTop = this.$refs.ul.offsetTop    // 动态获取ul距离顶部的宽高
+                const clientY = this.$refs["A"][0].offsetHeight // 动态获取每一个li到宽高
+                console.log(clientY);
+                const touchY = e.touches[0].clientY - eleOffsetTop;
+                const index = Math.floor((touchY - startY) / clientY); //当前手指滑动的位置
+                if (index >= 0 && this.citiesList.length) {
+                    this.$emit("change", this.citiesList[index]);
+                }
+            }
+        },
+        hiddleTouchend() {
+            this.touchStatus = false;
         }
-      }
-    },
-    hiddleTouchend() {
-      this.touchStatus = false;
     }
-  }
 };
 </script>
 
@@ -72,4 +70,3 @@ ul {
     }
 }
 </style>
-
